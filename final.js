d485e84ac0a4384837c0f77c375599ca37bdf09b
lorenac1 <lorena.cirincione@zhdk.ch>
2024-06-13 15:58:34 +0200

    Problem
    the <p> is overflowing

--- final.js
+++ final.js
@@ -27,7 +27,7 @@ function addTracksToPage(items) {
     const li = document.createElement("li");
     const albumImageUrl = item.track.album.images[0].url;
     li.innerHTML = `
-      <img src="${albumImageUrl}" alt="Album cover" style="width: 500px; height: 500px; border-radius: 16px;">
+      <img src="${albumImageUrl}" alt="Album cover" style="width: 600px; height: 600px; border-radius: 16px;">
       <p>${item.track.name} by ${item.track.artists.map((artist) => artist.name).join(", ")}</p>
       <audio controls src="${item.track.preview_url}"></audio>
     `;
